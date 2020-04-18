34007623da1942d60732b241e427f1232c9ac5a7
Logan Bauer <lbp1108@hotmail.com>
2020-04-17 23:09:30 -0300

    #ENC - Resolução desafio ReactJS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
+import { DeleteForeverOutlined, AddCircleOutlineOutlined } from '@material-ui/icons';
+import { Container, Row } from 'react-bootstrap';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
 
+import api from './services/api'
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('/repositories', {
+      title: `Novo repositório`,
+      url: "https://github.com/wdragow/conceitos-node.js",
+      techs: ["Node.js", "ReactJS"]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ));
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+      <Container>
+        <Row className="justify-content-center">
+            <button onClick={handleAddRepository}>
+              Adicionar
+              <AddCircleOutlineOutlined/>
+            </button>
+        </Row>
+        <Row className="justify-content-center">
+          <ul data-testid="repository-list">
+          {repositories.map(repository => (
+              <li key={repository.id}>
+                <h4>{repository.title}</h4>      
+                <a href={repository.url}>GitHub</a> 
+                <div>                
+                  {repository.techs.map(tech => (
+                    <h6 key={tech}>| {tech} |</h6>
+                  ))}
+                </div>
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                  <DeleteForeverOutlined/>
+                </button>             
+              </li>
+            ))}
+          </ul>
+        </Row>
+      </Container>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+    <header>
+      <h2>Conceitos ReactJS - GoStack</h2>
+    </header>
+  );
+} 
